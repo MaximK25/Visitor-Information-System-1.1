2299ed234d997dbf9978469aaf917e55fe633dca
MaximK25 <92697689+MaximK25@users.noreply.github.com>
2022-05-24 12:03:08 +1200

    Design

--- Visitor-Information-System 1.1/Form1.Designer.cs
+++ Visitor-Information-System 1.1/Form1.Designer.cs
@@ -225,7 +225,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(932, 579);
+            this.ClientSize = new System.Drawing.Size(932, 506);
             this.Controls.Add(this.lbl_Staff_ID);
             this.Controls.Add(this.TB_Staff_ID);
             this.Controls.Add(this.lbl_Visitor_ID);
